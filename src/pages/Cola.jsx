879d6c0d54a34bf4cdb0922d344e9ab63f31b3ea
demrebicer <demrebicer33@gmail.com>
2024-02-08 14:21:10 +0000

    update cola page

--- src/pages/Cola.jsx
+++ src/pages/Cola.jsx
@@ -1,14 +1,41 @@
 import React, { useRef, useState } from "react";
 import "../assets/styles/cola.scss";
 
-import colaCan from "../assets/images/colaCan.png";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+import { FaPlay } from "react-icons/fa6";
+import { FaPause } from "react-icons/fa6";
 
 function Cola() {
+  const [isAutoRotate, setIsAutoRotate] = useState(false);
+
+  function ColaCanModel() {
+    const gltf = useGLTF("/~db596/assets/colacan.gltf", true); // Modelin yolu
+    return <primitive object={gltf.scene} />;
+  }
+
+  // Function to toggle auto-rotate
+  const toggleAutoRotate = () => {
+    setIsAutoRotate(!isAutoRotate);
+  };
+
   return (
     <div className="cola">
       <h1 className="background-text">Coca</h1>
       <h1 className="background-text">Cola</h1>
-      <img className="cola-can" src={colaCan} alt="Cola Can" />
+      {/* <ColaCanImg /> */}
+      <Canvas className="cola-can-canvas" camera={{ position: [-1.5, 0, 4.5], fov: 40 }}>
+        <ambientLight intensity={1.5} /> {/* Çevresel ışığın yoğunluğunu 1.5 yapın */}
+        <directionalLight position={[0, 5, 0]} intensity={3} color={"#ffffff"} /> {/* Üstten gelen ışık */}
+        <directionalLight position={[0, -5, 0]} intensity={3} color={"#ffffff"} /> {/* Altından gelen ışığın yoğunluğunu artırın */}
+        <directionalLight position={[5, 0, 0]} intensity={0.5} color={"#ffffff"} /> {/* Sağdan gelen ışık */}
+        <directionalLight position={[-5, 0, 0]} intensity={0.5} color={"#ffffff"} /> {/* Soldan gelen ışık */}
+        <directionalLight position={[0, 0, 5]} intensity={0.5} color={"#ffffff"} /> {/* Önden gelen ışık */}
+        <directionalLight position={[0, 0, -5]} intensity={1} color={"#ffffff"} /> {/* Arkadan gelen ışık */}
+        <OrbitControls enableZoom={false} autoRotate={isAutoRotate}/>
+        <ColaCanModel />
+      </Canvas>
 
       <h1 className="title">Origin</h1>
       <h1 className="sub-title">Story</h1>
@@ -22,6 +49,18 @@ function Cola() {
         stewardship, recycling, and reducing its carbon footprint. Today, Coca-Cola stands as a symbol of refreshment, joy, and global
         unity, continuing to inspire moments of optimism and happiness for millions.
       </p>
+
+      <div className="control-buttons-container" onClick={toggleAutoRotate}>
+        {isAutoRotate ? (
+          <button className="control-button">
+            <FaPause size={24} />
+          </button>
+        ) : (
+          <button className="control-button">
+            <FaPlay size={24} />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
